25936a6632659dcd39e506881eeb81d0934b2fb7
Shubhang Gupta <guptashubhang2000@gmail.com>
2024-03-05 23:17:44 +0530

    fazeblog

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import { ModeToggle } from "./ModeToggle";
+
+export default function Navbar() {
+  return (
+    <nav className="w-full relative flex items-center justify-between max-w-2xl mx-auto px-4 py-5">
+      <Link href="/" className="font-bold text-3xl">
+        Faze<span className="text-blue-500">Blog</span>
+      </Link>
+
+      <ModeToggle />
+    </nav>
+  );
+}
