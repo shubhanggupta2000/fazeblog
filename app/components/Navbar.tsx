3db5431d6bf9d77c0a55ce675bde9eacb6b77166
Shubhang Gupta <guptashubhang2000@gmail.com>
2024-03-06 15:45:20 +0530

    navbar update

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { ModeToggle } from "./ModeToggle";
 
 export default function Navbar() {
   return (
-    <nav className="w-full relative flex items-center justify-between max-w-2xl mx-auto px-4 py-5">
+    <nav className="w-full relative flex items-center justify-between max-w-4xl mx-auto px-4 py-5">
       <Link href="/" className="font-bold text-3xl">
         Faze<span className="text-blue-500">Blog</span>
       </Link>
